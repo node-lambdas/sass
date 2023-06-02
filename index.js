dd4c40343203229df930a2f6e7c2026922c1042b
Darlan Alves <me@darlanalv.es>
2023-06-02 23:32:43 +0200

    chore: replace node-sass with sass

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-import sass from 'node-sass'
+import sass from "sass";
 
 export default {
   version: 2,
   actions: {
     toCSS: {
-      input: 'text',
-      output: 'text',
+      input: "text",
+      output: "text",
       handler: async function (input, output) {
         try {
-          const result = sass.renderSync({ data: await input.asText() });
-          output.sendText(200, result.css.toString());
+          const sassText = await input.asText();
+          const options = {
+            style: (input.options?.compressed && "compressed") || "expanded",
+          };
+
+          const result = sass.compileString(sassText, options);
+
+          output.sendText(200, String(result.css));
         } catch (e) {
           output.reject(e);
         }
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
