69a3bcb1d0114360ffacc6abae0521974645da32
Darlan Alves <me@darlanalv.es>
2023-06-20 14:48:32 +0200

    chore: add default flag

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ export default {
   version: 2,
   actions: {
     toCSS: {
+      default: true,
       input: "text",
       output: "text",
       handler: async function (input, output) {
         try {
-          const sassText = await input.asText();
+          const sassText = input.body;
           const options = {
             style: (input.options?.compressed && "compressed") || "expanded",
           };
-
           const result = sass.compileString(sassText, options);
-
+          
           output.sendText(200, String(result.css));
         } catch (e) {
           output.reject(e);
