0b0716030ada3f8e51fb9684af7f2eea7f1846a2
Darlan Alves <me@darlanalv.es>
2023-06-22 16:28:10 +0200

    chore: update index.js

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ export default {
       output: "text",
       handler: async function (input, output) {
         try {
-          const sassText = input.body;
           const options = {
             style: (input.options?.compressed && "compressed") || "expanded",
           };
-          const result = sass.compileString(sassText, options);
           
-          output.sendText(200, String(result.css));
+          const result = sass.compileString(input.body, options);
+          
+          output.send(200, String(result.css));
         } catch (e) {
+          console.error(e);
           output.reject(e);
         }
       },
